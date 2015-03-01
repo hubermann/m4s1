9270598aa42bd21742f4914375db92074570bb34
huber <huber@hubers-MacBook-Pro.local>
2015-03-01 03:45:30 -0300

    sec

---
+++ base.sql
@@ -0,0 +1,318 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 19, 2015 at 05:02 PM
+-- Server version: 5.5.40-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `masisa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `capacitaciones`
+--
+
+CREATE TABLE IF NOT EXISTS `capacitaciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL,
+  `fecha` date NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagenes_laempresa`
+--
+
+CREATE TABLE IF NOT EXISTS `imagenes_laempresa` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `laempresa_id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagenes_novedades`
+--
+
+CREATE TABLE IF NOT EXISTS `imagenes_novedades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `novedad_id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagenes_productos`
+--
+
+CREATE TABLE IF NOT EXISTS `imagenes_productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `producto_id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagenes_slider_servicios`
+--
+
+CREATE TABLE IF NOT EXISTS `imagenes_slider_servicios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `slider_servicio_id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laempresa`
+--
+
+CREATE TABLE IF NOT EXISTS `laempresa` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `texto_principal` text NOT NULL,
+  `texto_secundario` text NOT NULL,
+  `titulo_texto1` varchar(255) NOT NULL,
+  `texto1` text NOT NULL,
+  `titulo_texto2` varchar(255) NOT NULL,
+  `texto2` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `version` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`version`) VALUES
+(9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modulos`
+--
+
+CREATE TABLE IF NOT EXISTS `modulos` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `modulos`
+--
+
+INSERT INTO `modulos` (`id`, `nombre`) VALUES
+(1, 'permisos'),
+(2, 'roles'),
+(3, 'usuarios'),
+(4, 'laempresa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `novedades`
+--
+
+CREATE TABLE IF NOT EXISTS `novedades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `tags` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permisos`
+--
+
+CREATE TABLE IF NOT EXISTS `permisos` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL DEFAULT '0',
+  `role_id` int(2) NOT NULL,
+  `modulo` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `permiso` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `permisos`
+--
+
+INSERT INTO `permisos` (`id`, `status`, `role_id`, `modulo`, `url`, `permiso`) VALUES
+(1, 0, 1, '1', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productos`
+--
+
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int(11) NOT NULL,
+  `status` int(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `tags` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `status`, `nombre`) VALUES
+(1, 0, 'Administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `servicios`
+--
+
+CREATE TABLE IF NOT EXISTS `servicios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider_servicios`
+--
+
+CREATE TABLE IF NOT EXISTS `slider_servicios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `nombre_imagen` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sucursales`
+--
+
+CREATE TABLE IF NOT EXISTS `sucursales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL,
+  `mapa` text NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `localidad` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `telefono` varchar(120) NOT NULL,
+  `telefono2` varchar(120) NOT NULL,
+  `mail` varchar(120) NOT NULL,
+  `horario` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL DEFAULT '0',
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `salt` varchar(100) NOT NULL,
+  `role_id` int(2) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `status`, `nombre`, `apellido`, `email`, `password`, `salt`, `role_id`, `created_at`, `updated_at`, `filename`) VALUES
+(1, 0, 'super', 'user', 'admin@mail.com', 'd715e5d5f7449f8914cf7f19ea8643ba4814cc7f13853785f910ca6beb13aa0b860f7856b50790231c1b21a168d81308cf389d8605c75e3c9eabff3e4f0966a2', '963da0c4b89504217d036a315c1b88c2', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
